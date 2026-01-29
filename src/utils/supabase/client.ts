66e38cca9c64e666f70fa0df39cc8b005751e3ba
노나연 <norankoj@naver.com>
2026-01-29 10:50:35 +0900

    콘솔로그 제거

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -2,9 +2,6 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
-  console.log("Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-  console.log("KEY 확인:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
