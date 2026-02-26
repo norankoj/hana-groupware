6a5cf498ba5d14a8016b5bb4fe4acb02ff91584b
나연 <norankoj@naver.com>
2026-02-26 10:38:05 +0900

    fix: 로그인 쿠키 유지 시간 1년으로 설정

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -5,5 +5,15 @@ export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookieOptions: {
+        // 쿠키 유지 시간을 1년으로 설정
+        maxAge: 60 * 60 * 24 * 365,
+        domain: "",
+        path: "/",
+        sameSite: "lax",
+        secure: true,
+      },
+    },
   );
 }
